af30fdec8993e5ddb78f426e09da71c33d1024cf
longrui <18813867725@163.com>
2021-04-08 17:27:24 +0800

    Initial commit

---
+++ src/test/java/com/bmb/compute/bioinfo/BioinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bmb.compute.bioinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BioinfoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
